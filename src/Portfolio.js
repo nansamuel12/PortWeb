b221a7bde1232640d5f30800cf5e8c77bcc808c0
nansamuel12 <nansamuel12@gmail.com>
2025-01-13 01:12:22 +0300

    Your descriptive commit message

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -8,7 +8,7 @@ const Portfolio = () => {
         <div className="portfolio-card">
         <img src="/asset/Rectangle 8.png" alt="icon" />
           <p>
-            It is a long established fact that a reader will be distracted 
+           Hanna Samuel fact that a reader will be distracted 
             by the readable content of a page when looking at its layout. 
             The point of using Lorem Ipsum is that it has a more-or-less 
             normal distribution of letters, as opposed to using.
@@ -19,7 +19,7 @@ const Portfolio = () => {
         <div className="portfolio-card">
         <img src="/asset/Rectangle 8.png" alt="icon" />
           <p>
-            It is a long established fact that a reader will be distracted 
+          Hanna Samuel that a reader will be distracted 
             by the readable content of a page when looking at its layout. 
             The point of using Lorem Ipsum is that it has a more-or-less 
             normal distribution of letters, as opposed to using.
@@ -30,7 +30,7 @@ const Portfolio = () => {
         <div className="portfolio-card">
         <img src="/asset/Rectangle 8.png" alt="icon" />
           <p>
-            It is a long established fact that a reader will be distracted 
+           Hanna Samueeu fact that a reader will be distracted 
             by the readable content of a page when looking at its layout. 
             The point of using Lorem Ipsum is that it has a more-or-less 
             normal distribution of letters, as opposed to using.
